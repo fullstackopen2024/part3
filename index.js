f90bebf1ce93870f7906e13f9d85642a5342d4c6
Vlad-Rares Raducu <vlad.raducu@wisercat.net>
2024-06-24 10:50:16 +0300

    [phonebook] [3.4] Solved exercise 3.4 phonebook, step 4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const phonebooks = [
+let phonebooks = [
     {
         "id": "1",
         "name": "Arto Hellas",
@@ -61,6 +61,14 @@ app.get('/api/persons/:id', (req, res) => {
     } else {
         res.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+
+    phonebooks = phonebooks.filter(note => note.id !== id)
+
+    res.status(204).end()
 })
 
 const PORT = 3001
